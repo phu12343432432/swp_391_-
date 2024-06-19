d228a8e605a5365d6b298c873f68e7ea72a897be
Phucng <phucnguyenp93@gmail.com>
2024-06-19 21:54:57 +0700

    Update UserTeamController

--- src/java/Controller/UserTeamController.java
+++ src/java/Controller/UserTeamController.java
@@ -4,9 +4,9 @@
  */
 package Controller;
 
-import DAO.ProfileDAO;
 import DAO.TeamDAO;
 import Model.Team;
+import Model.Team_Member;
 import Model.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
@@ -16,6 +16,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.Part;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -39,13 +41,23 @@ public class UserTeamController extends HttpServlet {
                 case "team-history":
                     viewHistory(request, response);
                     break;
-                case "view-team":
-                    viewTeamDetails(request, response);
+                case "delete-team-member":
+                    deleteTeamMember(request, response);
                     break;
+
             }
         } else {
-            url = "views/common/sign-in.jsp";
-            request.getRequestDispatcher("views/common/sign-in.jsp").forward(request, response);
+            boolean isRedirect = false;
+            switch (action) {
+                case "view-team":
+                    viewTeamDetails(request, response);
+                    isRedirect = true;
+                    break;
+            }
+            if (!isRedirect) {
+                url = "views/common/sign-in.jsp";
+                request.getRequestDispatcher("views/common/sign-in.jsp").forward(request, response);
+            }
         }
     }
 
@@ -59,6 +71,12 @@ public class UserTeamController extends HttpServlet {
             case "update":
                 updateTeam(request, response);
                 break;
+            case "add-team-member":
+                addTeamMember(request, response);
+                break;
+            case "update-team-member":
+                updateTeamMember(request, response);
+                break;
         }
 
     }
@@ -68,10 +86,35 @@ public class UserTeamController extends HttpServlet {
             HttpSession session = request.getSession();
             User user = (User) session.getAttribute("USER"); // object
             TeamDAO teamDAO = new TeamDAO();
-            Team userTeam = teamDAO.getTeamByUserId(user.getId());
-            if (userTeam != null) {
-                request.setAttribute("TEAM", userTeam); // set value => ton tai trong 1 request.
+            String indexS = request.getParameter("index");
+            String searchS = request.getParameter("search");
+            if (indexS == null) {
+                indexS = "1";
             }
+            if (searchS == null) {
+                searchS = "";
+            }
+            int index = Integer.parseInt(indexS);
+            int total = teamDAO.getListTeamMemberByTeamIdTotal(user.getId());
+            Team userTeam = teamDAO.getTeamByUserId(user.getId());
+            List<Team_Member> teamMember = new ArrayList();
+            if (userTeam != null) {
+                teamMember = teamDAO.getListTeamMemberByTeamId(userTeam.getId(), index);
+                request.setAttribute("TEAM", userTeam);
+                request.setAttribute("TEAM_MEMBERS", teamMember);
+                request.setAttribute("userTeamId", userTeam.getId());
+            }
+            if (searchS != "") {
+                total = teamDAO.getListTeamMemberByTeamIdSeachTotal(userTeam.getId(), searchS);
+                teamMember = teamDAO.getListTeamMemberByTeamIdSearch(userTeam.getId(), searchS, index);
+                request.setAttribute("search", searchS);
+            }
+            int lastPage = total / 10;
+            if (total % 10 != 0) {
+                lastPage++;
+            }
+            request.setAttribute("endP", lastPage);
+            request.setAttribute("selectedPage", index);
             request.getRequestDispatcher("views/user/user-team.jsp").forward(request, response);
         } catch (Exception e) {
             e.printStackTrace();
@@ -83,20 +126,17 @@ public class UserTeamController extends HttpServlet {
             HttpSession session = request.getSession(false);
             User userLogin = (User) session.getAttribute("USER");
 
-            
             // mac dinh kieu tra ve cua request.getParam => Auto String.
             String teamName = request.getParameter("teamName");
             String shortName = request.getParameter("shortName");
             String teamsizeS = request.getParameter("teamsize");
             String teamIdS = request.getParameter("teamId");
             String desc = request.getParameter("desc");
-            
+
             // lu anh dan binary
             Part image = request.getPart("image");
             // => local D:/...
 
-            
-          
             TeamDAO teamDAO = new TeamDAO();
             Team _team = new Team();
             _team.setName(teamName);
@@ -104,14 +144,13 @@ public class UserTeamController extends HttpServlet {
             _team.setTeamSize(Integer.parseInt(teamsizeS));
             _team.setDescription(desc);
             _team.setUserId(userLogin.getId());
-            
+
             Team teamUpdated = new Team();
             // ton tai teamid co team roi.
             if (!(teamIdS.equals(""))) {
                 // goi phunog thuc DAO update team.
                 int teamId = Integer.parseInt(teamIdS);
                 _team.setId(teamId);
-                System.out.println("TeamId " + teamId);
                 // Thang vua duoc update
                 teamUpdated = teamDAO.updateTeam(_team, image);
             } else {
@@ -141,13 +180,92 @@ public class UserTeamController extends HttpServlet {
             HttpSession session = request.getSession();
             String team = request.getParameter("id");
             int teamId = Integer.parseInt(team);
-            User user = (User) session.getAttribute("USER");
             TeamDAO teamDAO = new TeamDAO();
             Team userTeam = teamDAO.getTeamById(teamId);
             if (userTeam != null) {
                 request.setAttribute("TEAM", userTeam);
             }
             request.getRequestDispatcher("views/user/team-details.jsp").forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addTeamMember(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String url = "team";
+            HttpSession session = request.getSession(false);
+            User userLogin = (User) session.getAttribute("USER");
+
+            // mac dinh kieu tra ve cua request.getParam => Auto String.
+            String name = request.getParameter("name");
+            String numberS = request.getParameter("number");
+            String teamIdS = request.getParameter("teamId");
+
+            Team_Member teamMember = new Team_Member();
+            teamMember.setName(name);
+            teamMember.setNumber(Integer.parseInt(numberS));
+            teamMember.setTeamId(Integer.parseInt(teamIdS));
+
+            TeamDAO teamDAO = new TeamDAO();
+            boolean result = teamDAO.AddTeam_Member(teamMember);
+            if (result) {
+                request.setAttribute("MESSAGE", "Add team member sucessfully");
+            } else {
+                request.setAttribute("ERROR", "Add team member failed");
+            }
+            response.sendRedirect(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteTeamMember(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String url = "team";
+            HttpSession session = request.getSession(false);
+            User userLogin = (User) session.getAttribute("USER");
+
+            // mac dinh kieu tra ve cua request.getParam => Auto String.
+            String id = request.getParameter("id");
+            int teamMemberId = Integer.parseInt(id);
+
+            TeamDAO teamDAO = new TeamDAO();
+            boolean result = teamDAO.DeleteTeam_Member(teamMemberId);
+            if (result) {
+                request.setAttribute("MESSAGE", "Delete team member sucessfully");
+            } else {
+                request.setAttribute("ERROR", "Delete team member failed");
+            }
+            response.sendRedirect(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void updateTeamMember(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String url = "team";
+            HttpSession session = request.getSession(false);
+            User userLogin = (User) session.getAttribute("USER");
+
+            // mac dinh kieu tra ve cua request.getParam => Auto String.
+            String name = request.getParameter("name");
+            String numberS = request.getParameter("number");
+            String ids = request.getParameter("id");
+
+            Team_Member teamMember = new Team_Member();
+            teamMember.setName(name);
+            teamMember.setNumber(Integer.parseInt(numberS));
+            teamMember.setId(Integer.parseInt(ids));
+            TeamDAO teamDAO = new TeamDAO();
+            boolean result = teamDAO.UpdateTeam_Member(teamMember);
+            if (result) {
+                request.setAttribute("MESSAGE", "Add team member sucessfully");
+            } else {
+                request.setAttribute("ERROR", "Add team member failed");
+            }
+            response.sendRedirect(url);
         } catch (Exception e) {
             e.printStackTrace();
         }

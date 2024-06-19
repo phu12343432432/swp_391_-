8244722085a1a8589baf7b1ebd7456c8f9e7d6a6
Phucng <phucnguyenp93@gmail.com>
2024-06-19 21:56:03 +0700

    Update TeamDAO

--- src/java/DAO/TeamDAO.java
+++ src/java/DAO/TeamDAO.java
@@ -6,6 +6,7 @@ package DAO;
 
 import DAL.DBContext;
 import Model.Team;
+import Model.Team_Member;
 import Model.User;
 import jakarta.servlet.http.Part;
 import java.io.InputStream;
@@ -15,6 +16,7 @@ import java.sql.ResultSet;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
@@ -39,7 +41,7 @@ public class TeamDAO extends DBContext {
         }
     }
 
-     public Team getTeamById(int _teamId) {
+    public Team getTeamById(int _teamId) {
         try {
             String sql = "SELECT * FROM Team WHERE Id = ?";
             ps = con.prepareStatement(sql);
@@ -68,7 +70,7 @@ public class TeamDAO extends DBContext {
         }
         return null;
     }
-    
+
     public Team getTeamByUserId(int userId) {
         try {
             String sql = "SELECT * FROM Team WHERE UserId = ?";
@@ -99,7 +101,6 @@ public class TeamDAO extends DBContext {
         return null;
     }
 
-   
     public Team updateTeam(Team team, Part image) {
         try {
             Team _team = new Team();
@@ -147,7 +148,7 @@ public class TeamDAO extends DBContext {
                     int teamId = rs.getInt("Id");
                     String Name = rs.getString("Name");
                     String ShortName = rs.getString("ShortName");
-                    String Description = rs.getString("Description");  
+                    String Description = rs.getString("Description");
                     int teamSize = rs.getInt("TeamSize");
 
                     byte[] imgData = rs.getBytes("Image");
@@ -188,6 +189,144 @@ public class TeamDAO extends DBContext {
             ps.setInt(7, team.getUserId());
             ps.setBoolean(8, true);
 
+            int affectedRow = ps.executeUpdate();
+            return affectedRow > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public int getListTeamMemberByTeamIdSeachTotal(int teamId, String search) {
+        try {
+            List<Team_Member> listTeamMember = new ArrayList<>();
+            String sql = "SELECT COUNT(*) FROM Team_Member WHERE TeamId = ? AND Name LIKE ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, teamId);
+            ps.setString(2, "%" + search + "%");
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<Team_Member> getListTeamMemberByTeamIdSearch(int teamId, String search, int index) {
+        try {
+            List<Team_Member> listTeamMember = new ArrayList<>();
+            String sql = "SELECT * FROM Team_Member WHERE TeamId = ? AND Name LIKE ? ORDER BY Id DESC OFFSET ? ROW FETCH NEXT 10 ROWS ONLY";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, teamId);
+            ps.setString(2, "%" + search + "%");
+            ps.setInt(3, (index - 1) * 10);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Team_Member teamMember = new Team_Member();
+                String Name = rs.getString("Name");
+                int Number = rs.getInt("Number");
+                int TeamId = rs.getInt("TeamId");
+                int Id = rs.getInt("Id");
+
+                teamMember.setId(Id);
+                teamMember.setName(Name);
+                teamMember.setNumber(Number);
+                teamMember.setTeamId(TeamId);
+                listTeamMember.add(teamMember);
+            }
+            return listTeamMember;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int getListTeamMemberByTeamIdTotal(int teamId) {
+        try {
+            List<Team_Member> listTeamMember = new ArrayList<>();
+            String sql = "SELECT COUNT(*) FROM Team_Member WHERE TeamId = ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, teamId);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<Team_Member> getListTeamMemberByTeamId(int teamId, int index) {
+        try {
+            List<Team_Member> listTeamMember = new ArrayList<>();
+            String sql = "SELECT * FROM Team_Member WHERE TeamId = ? ORDER BY Id DESC OFFSET ? ROW FETCH NEXT 10 ROWS ONLY";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, teamId);
+            ps.setInt(2, (index - 1) * 10);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Team_Member teamMember = new Team_Member();
+                String Name = rs.getString("Name");
+                int Number = rs.getInt("Number");
+                int TeamId = rs.getInt("TeamId");
+                int Id = rs.getInt("Id");
+
+                teamMember.setId(Id);
+                teamMember.setName(Name);
+                teamMember.setNumber(Number);
+                teamMember.setTeamId(TeamId);
+                listTeamMember.add(teamMember);
+            }
+            return listTeamMember;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    
+    
+    public boolean AddTeam_Member(Team_Member teamMember) {
+        try {
+            String sql = "INSERT  dbo.[Team_Member] ([Name], [Number], [TeamId]) VALUES (?, ?, ?)";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, teamMember.getName());
+            ps.setInt(2, teamMember.getNumber());
+            ps.setInt(3, teamMember.getTeamId());
+            int affectedRow = ps.executeUpdate();
+            return affectedRow > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean UpdateTeam_Member(Team_Member teamMember) {
+        try {
+            String sql = "UPDATE dbo.[Team_Member] SET Name = ?, Number = ? WHERE Id = ?";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, teamMember.getName());
+            ps.setInt(2, teamMember.getNumber());
+            ps.setInt(3, teamMember.getId());
+            int affectedRow = ps.executeUpdate();
+            return affectedRow > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean DeleteTeam_Member(int id) {
+        try {
+            String sql = "DELETE  dbo.[Team_Member]  WHERE Id = ?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
             int affectedRow = ps.executeUpdate();
             return affectedRow > 0;
         } catch (Exception e) {
